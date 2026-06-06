0df19462ac954c9ab63353e974281306cbb9f9d1
Shihab Mahamud <shihab4t@gmail.com>
2026-06-07 01:20:36 +0600

    example project

---
+++ tests/example/src/app.tsx
@@ -0,0 +1,11 @@
+import { MyButton } from './components/my-button';
+import { Counter } from './components/counter';
+
+export const App = () => {
+    return (
+        <>
+            <MyButton />
+            <Counter />
+        </>
+    );
+};
---
+++ tests/example/src/pages/about.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+    return <div>Edit: `src/pages/about.tsx`</div>;
+};
+
+export default About;
