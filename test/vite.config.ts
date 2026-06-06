4b8e9905b460c26d2f2689501fcfe57d4d2eaada
Shihab Mahamud <shihab4t@gmail.com>
2026-06-06 23:22:56 +0600

    chore: import plugin via package name in test project

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { boilerplate } from '../src/index';
+import { boilerplate } from 'vite-plugin-boilerplate';
 
 // https://vite.dev/config/
 export default defineConfig({
